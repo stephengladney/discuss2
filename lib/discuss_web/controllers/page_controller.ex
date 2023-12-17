b47485823cad4d55dc7c57a14d8ff7b9a13e9caf
Stephen Gladney <stephengladney@gmail.com>
2023-12-16 22:08:19 -0500

    initial commit with files

---
+++ lib/discuss_web/controllers/page_controller.ex
@@ -0,0 +1,13 @@
+defmodule DiscussWeb.PageController do
+  use DiscussWeb, :controller
+
+  def home(conn, _params) do
+    # The home page is often custom made,
+    # so skip the default app layout.
+    render(conn, :home, layout: false)
+  end
+
+  def hello(conn, _params) do
+    text(conn, "HELLO!")
+  end
+end
